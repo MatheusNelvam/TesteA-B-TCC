66962a29f1de59274277f0709111c8e99d3256f0
MatheusNelvam <matheus.nelvam@gmail.com>
2026-07-10 09:55:13 -0300

    configure whitenoise to serve static files in production

--- core/settings.py
+++ core/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -135,3 +136,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+# Configuração do WhiteNoise para servir estáticos em produção
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
